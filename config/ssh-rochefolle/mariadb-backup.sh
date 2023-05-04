05d7b9e92482158b3eec29ab11e9ffec337c4dab
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2023-05-04 22:49:30 +0700

    Add rclone to copy backup to Dropbox

--- config/ssh-rochefolle/mariadb-backup.sh
+++ config/ssh-rochefolle/mariadb-backup.sh
@@ -4,6 +4,8 @@ FILENAME=$(date '+%Y%m%d-%H%M%S')
 
 mysqldump -h narok-wp-mariadb -unarok-wp-user -p"<NAROK WP PASSWORD>" narok-wp-db > /data/narok-wp-${FILENAME}.sql
 gzip /data/narok-wp-${FILENAME}.sql
+rclone move /data/narok-wp-${FILENAME}.sql.gz dropbox:backup/narokio/
 
 mysqldump -h hdc-wp-mariadb -uhdc-wp-user -p"<HDC WP PASSWORD>" hdc-wp-db > /data/hdc-wp-${FILENAME}.sql
 gzip /data/hdc-wp-${FILENAME}.sql
+rclone move /data/hdc-wp-${FILENAME}.sql.gz dropbox:backup/himdome/
