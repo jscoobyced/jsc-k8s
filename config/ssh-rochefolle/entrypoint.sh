3234d8f4403e1dcdaf972bee68492eecb69b184f
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2023-05-04 23:46:36 +0700

    Enable cron to backup WP databases

--- config/ssh-rochefolle/entrypoint.sh
+++ config/ssh-rochefolle/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+touch /var/log/cron.log
+chmod o+r /var/log/cron.log
+crond -l 2 -L /var/log/cron.log
+
 ssh-keygen -A
 exec /usr/sbin/sshd -D -e "$@"
--- config/ssh-rochefolle/mariadb-backup.sh
+++ config/ssh-rochefolle/mariadb-backup.sh
@@ -2,6 +2,12 @@
 
 FILENAME=$(date '+%Y%m%d-%H%M%S')
 
+if [ ! -d ~/.config/rclone ];
+then
+    mkdir -p ~/.config/rclone/
+    cp /data/.config/rclone/rclone.conf ~/.config/rclone/rclone.conf
+fi
+
 mysqldump -h narok-wp-mariadb -unarok-wp-user -p"<NAROK WP PASSWORD>" narok-wp-db > /data/narok-wp-${FILENAME}.sql
 gzip /data/narok-wp-${FILENAME}.sql
 rclone move /data/narok-wp-${FILENAME}.sql.gz dropbox:backup/narokio/
