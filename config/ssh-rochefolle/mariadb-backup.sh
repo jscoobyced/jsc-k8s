914f3aa0368e6cf6e5c039b7bef6c223165171de
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2023-05-05 16:54:53 +0700

    Update timezone and improve backup script.

--- config/ssh-rochefolle/mariadb-backup.sh
+++ config/ssh-rochefolle/mariadb-backup.sh
@@ -1,17 +1,20 @@
 #!/bin/ash
 
-FILENAME=$(date '+%Y%m%d-%H%M%S')
+VERSION=$(date '+%Y%m%d-%H%M%S')
+DROPBOX=/data/dropbox/backup
+NAROK=${DROPBOX}/narokio
+HDC=${DROPBOX}/himdome
 
-if [ ! -d ~/.config/rclone ];
-then
-    mkdir -p ~/.config/rclone/
-    cp /data/.config/rclone/rclone.conf ~/.config/rclone/rclone.conf
-fi
+mkdir -p ${NAROK}
+mkdir -p ${HDC}
 
-mysqldump -h narok-wp-mariadb -unarok-wp-user -p"<NAROK WP PASSWORD>" narok-wp-db > /data/narok-wp-${FILENAME}.sql
-gzip /data/narok-wp-${FILENAME}.sql
-rclone move /data/narok-wp-${FILENAME}.sql.gz dropbox:backup/narokio/
+find ${NAROK}/* -mtime +31 -exec rm {} \;
+find ${HDC}/* -mtime +31 -exec rm {} \;
 
-mysqldump -h hdc-wp-mariadb -uhdc-wp-user -p"<HDC WP PASSWORD>" hdc-wp-db > /data/hdc-wp-${FILENAME}.sql
-gzip /data/hdc-wp-${FILENAME}.sql
-rclone move /data/hdc-wp-${FILENAME}.sql.gz dropbox:backup/himdome/
+mysqldump -h narok-wp-mariadb -unarok-wp-user -p"<NAROK WP PASSWORD>" narok-wp-db > /data/narok-wp-${VERSION}.sql
+gzip /data/narok-wp-${VERSION}.sql
+mv /data/narok-wp-${VERSION}.sql.gz ${NAROK}
+
+mysqldump -h hdc-wp-mariadb -uhdc-wp-user -p"<HDC WP PASSWORD>" hdc-wp-db > /data/hdc-wp-${VERSION}.sql
+gzip /data/hdc-wp-${VERSION}.sql
+mv /data/hdc-wp-${VERSION}.sql.gz ${HDC}
