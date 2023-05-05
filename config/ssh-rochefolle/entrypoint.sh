a4f061ba26764d3ed71f1d3d13cf0d8d41d7a8ad
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2023-05-05 17:02:24 +0700

    Improve entrypoint of backup server

--- config/ssh-rochefolle/entrypoint.sh
+++ config/ssh-rochefolle/entrypoint.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
+# Setup and start cron
 touch /var/log/cron.log
 chmod o+r /var/log/cron.log
 crond -l 2 -L /var/log/cron.log
 
+# Setup and mount dropbox with rclone
+su-exec jscoobyced cp -r /data/.config /home/jscoobyced
+rclone mount --daemon dropbox: /data/dropbox
+
+# Ensure backup script has password
+NOPASSWD=$(grep "WP PASSWORD" /home/jscoobyced/bin/mariadb-backup.sh | wc -l)
+
+if [ "${NOPASSWD}" != "0" ];
+then
+    su-exec jscoobyced cp /data/mariadb-backup.sh /home/jscoobyced/bin/
+fi    
+
+# Setup and start SSH
 ssh-keygen -A
 exec /usr/sbin/sshd -D -e "$@"
